5ce4a355c68644228c987e2b4648b7ade8b66542
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-03-25 12:29:05 -0300

    app

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import { readdir } from "fs"
 import { applyAssistance } from "./utils/database.js"
 import { parseAssistances } from "./utils/file.js"
 
-readdir("chats", { encoding: "utf-8" }, (err, chatFiles) => {
-  if (err) return console.log("There was an error reading the directory!")
+const app = () => {
+  readdir("chats", { encoding: "utf-8" }, (err, chatFiles) => {
+    if (err) return console.log("There was an error reading the directory!")
+  
+    chatFiles?.forEach((filename) => parseAssistances(filename, applyAssistance))
+  })
+}
 
-  chatFiles?.forEach((filename) => parseAssistances(filename, applyAssistance))
-})
+app()
