dfd9171d9ce7035f9716f983452e3aeb591985ef
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-03-25 23:36:58 -0300

    optimizations

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { readdir } from "fs"
 import { parseAssistances } from "./utils/file.js"
 import { applyAssistance } from "./utils/database.js"
 
-readdir("chats", { encoding: "utf-8" }, (err, chatFiles) => {
+readdir("chats", { encoding: "utf-8" }, (err, filenames) => {
   if (err) return console.log("There was an error reading the directory!")
   
-  parseAssistances(chatFiles[-1], applyAssistance)
+  parseAssistances(filenames[-1], applyAssistance)
 })
