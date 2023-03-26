3a346637866d5c96acface1d37c0ae50fae2de9d
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-03-25 23:32:49 -0300

    optimization

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ import { applyAssistance } from "./utils/database.js"
 readdir("chats", { encoding: "utf-8" }, (err, chatFiles) => {
   if (err) return console.log("There was an error reading the directory!")
   
-  chatFiles?.forEach((filename) => parseAssistances(filename, applyAssistance))
+  parseAssistances(chatFiles[-1], applyAssistance)
 })
